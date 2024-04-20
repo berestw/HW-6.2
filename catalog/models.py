8cd05a0e5cc12e11235f2b1fd00f10e39b5f17d1
berestw <paladin.berestw@yandex.ru>
2024-04-20 17:31:33 +0300

    6.3 (fix)

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -17,27 +18,26 @@ class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Наименование")
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
     image = models.ImageField(
-        upload_to="catalog/preview",
+        upload_to="catalog/",
         blank=True,
         null=True,
         verbose_name="Изображение (превью)",
     )
     category = models.ForeignKey(
         Category,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         blank=True,
         null=True,
         verbose_name="Категория",
         related_name="categories",
     )
-    price = models.IntegerField(verbose_name="Цена за покупку")
-    creation_at = models.DateTimeField(verbose_name="Дата создания (записи в БД)")
-    updated_at = models.DateTimeField(
-        verbose_name="Дата последнего изменения (записи в БД)"
+    price = models.FloatField(verbose_name="Цена за покупку")
+    creation_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания (записи в БД)")
+    updated_at = models.DateTimeField(default=timezone.now, verbose_name="Дата последнего изменения (записи в БД)"
     )
 
     def __str__(self):
-        return self.name
+        return f'{self.name} {self.price}'
 
     class Meta:
         verbose_name = "Продукт"
